fd512f96a376087f43308202d95a04e94b4ea469
Ayesha Azad <ayeshaazad@Ayeshas-MacBook-Pro.local>
2026-01-20 12:45:06 +0500

    Checking environment variables

---
+++ script.sh
@@ -0,0 +1,3 @@
+echo "This a test script to check environment variables like $APP_ENV"
+echo "export $APP_ENV=dev"
+echo "Above is the syntax for passing environment variables in Mac without commas."
